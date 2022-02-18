c980dc1b435a73724380930b81996fc9567ea7ec
t-shinkai00 <t.shinkai00@gmail.com>
2022-02-18 14:36:04 +0900

    define service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Todo } from './models/todo.models';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  // 今回はDBと接続しないのでメモリ上にTodoを保存
+  private todos: Todo[] = [];
+
+  // 全件取得のメソッド
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  // idを元に一件取得のメソッド
+  findById(id: string): Todo {
+    const result = this.todos.find((todo) => id === todo.id);
+
+    // なかったら404エラー
+    if (!result) {
+      throw new NotFoundException();
+    }
+    return result;
+  }
+}
