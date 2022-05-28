680a01aaf65489a9bee420e1569696f1fab48d78
t-shinkai00 <t.shinkai00@gmail.com>
2022-05-28 13:11:24 +0900

    set ApolloDriver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       // schemaファイルのパスを指定
       autoSchemaFile: join(process.cwd(), 'src/shema.gql'),
       // 生成されたschemaを自動でsortされるためのオプションをオンにする
