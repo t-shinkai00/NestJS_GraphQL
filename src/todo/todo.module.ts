3293574a3c9bb1efd03003a56a8bcc11b8382fd9
t-shinkai00 <t.shinkai00@gmail.com>
2022-05-28 12:30:06 +0900

    remove

--- src/todo/todo.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TodoService } from './todo.service';
-import { TodoResolver } from './todo.resolver';
-
-@Module({
-  providers: [TodoService, TodoResolver]
-})
-export class TodoModule {}
